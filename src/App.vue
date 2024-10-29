19433a6b3913d6e4ebe1001acadbe6c077d0ae75
ValerikGl <valeri.gladunik@tptlive.ee>
2024-10-29 18:43:13 +0200

    search bar added

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,40 @@
 
 <template>
   <v-app theme="dark">
-    <v-app-bar :elevation="2">
+    <v-app-bar :elevation="2" style="height: 75px">
       <template v-slot:prepend>
         <v-app-bar-nav-icon></v-app-bar-nav-icon>
       </template>
-            <v-img
-              max-width="30"
-              max-height="40"
-              cover
-              src="https://upload.wikimedia.org/wikipedia/commons/0/09/YouTube_full-color_icon_%282017%29.svg" style="margin-left: 50px; margin-right: -15px;"
-            ></v-img>
-      <v-app-bar-title style="font-weight: 600">YouTube</v-app-bar-title>
+      <v-img
+        max-width="30"
+        max-height="40"
+        cover
+        src="https://upload.wikimedia.org/wikipedia/commons/0/09/YouTube_full-color_icon_%282017%29.svg"
+        style="margin-left: -800px; margin-right: -15px"
+      ></v-img>
+      <v-app-bar-title
+        style="font-weight: 600; max-width: 100px; margin-right: -650px"
+        >YouTube</v-app-bar-title
+      >
+
+      <v-autocomplete
+        :items="items"
+        class="mx-auto"
+        density="comfortable"
+        menu-icon="mdi-magnify"
+        placeholder="Search"
+        style="max-width: 650px; margin-top: -5px"
+        theme="dark"
+        variant="solo"
+        auto-select-first
+        item-props
+        rounded
+      ></v-autocomplete>
     </v-app-bar>
 
     <v-card>
       <v-layout>
-        <v-navigation-drawer expand-on-hover rail>
-          <v-list>
-            <v-list-item prepend-icon="mdi-menu" title="YouTube"></v-list-item>
-          </v-list>
-
+        <v-navigation-drawer expand-on-hover rail style="margin-top: 75px">
           <v-list density="compact" nav>
             <v-list-item
               prepend-icon="mdi-home"
@@ -49,7 +63,7 @@
           </v-list>
         </v-navigation-drawer>
 
-        <v-main style="height: 550px"></v-main>
+        <v-main style="height: 100vh"></v-main>
       </v-layout>
     </v-card>
   </v-app>
